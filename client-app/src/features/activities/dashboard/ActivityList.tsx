5b364156d4f343a3ac0aa6851d68bc4b2f3e33f5
unknown <tromavoto@abv.bg>
2020-01-13 17:44:55 +0200

    Axios updates and added loading components

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,46 +1,52 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { Item, Button, Label, Segment } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 
 interface IProps {
   activities: IActivity[];
   selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
+  deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+  submitting: boolean;
+  target: string;
 }
 
 export const ActivityList: React.FC<IProps> = ({
   activities,
   selectActivity,
-  deleteActivity
+  deleteActivity,
+  submitting,
+  target
 }) => {
   return (
     <Segment clearing>
       <Item.Group divided>
-        {activities.map(acitivity => (
-          <Item key={acitivity.id}>
+        {activities.map(activity => (
+          <Item key={activity.id}>
             <Item.Content>
-              <Item.Header as="a">{acitivity.title}</Item.Header>
-              <Item.Meta>{acitivity.date}</Item.Meta>
+              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Meta>{activity.date}</Item.Meta>
               <Item.Description>
-                <div>{acitivity.description}</div>
+                <div>{activity.description}</div>
                 <div>
-                  {acitivity.city}, {acitivity.venue}
+                  {activity.city}, {activity.venue}
                 </div>
               </Item.Description>
               <Item.Extra>
                 <Button
-                  onClick={() => selectActivity(acitivity.id)}
+                  onClick={() => selectActivity(activity.id)}
                   floated="right"
                   content="View"
                   color="blue"
                 />
                 <Button
-                  onClick={() => deleteActivity(acitivity.id)}
+                  name={activity.id}
+                  loading={target === activity.id && submitting}
+                  onClick={(e) => deleteActivity(e, activity.id)}
                   floated="right"
                   content="Delete"
                   color="red"
                 />
-                <Label basic content={acitivity.category} />
+                <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
           </Item>
